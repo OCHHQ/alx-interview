4fd157b7f8e6d5ad0f4ba31a1592db223318e827
OCHHQ <enosejecollins@gmail.com>
2024-11-08 15:35:26 +0100

    utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,34 +1,32 @@
 #!/usr/bin/python3
-
 def validUTF8(data):
-    # Number of bytes in the current UTF-8 character
+    # Number of bytes remaining for the current character
     num_bytes = 0
 
-    # Masks to check the most significant bits of each byte
-    mask1 = 1 << 7  # 10000000
-    mask2 = 1 << 6  # 01000000
+    # Masks to determine the most significant bits
+    mask1 = 1 << 7  # 10000000 in binary
+    mask2 = 1 << 6  # 01000000 in binary
 
     for num in data:
-        # Get the 8 least significant bits of the integer
+        # Get the 8 least significant bits (simulate a byte)
         byte = num & 0xFF
 
         if num_bytes == 0:
-            # Determine how many bytes the current character should have
-            mask = 1 << 7
-            while mask & byte:
-                num_bytes += 1
-                mask = mask >> 1
-
-            # 1-byte characters (0xxxxxxx) or an invalid (more than 4 bytes)
-            if num_bytes == 0:
-                continue
-            if num_bytes == 1 or num_bytes > 4:
-                return False
+            # Count the number of leading 1's to determine the byte length
+            if byte & mask1 == 0:
+                continue  # 1-byte character (0xxxxxxx)
+            elif byte & (mask1 >> 1) == mask1:
+                num_bytes = 1  # 2-byte character
+            elif byte & (mask1 >> 2) == mask1:
+                num_bytes = 2  # 3-byte character
+            elif byte & (mask1 >> 3) == mask1:
+                num_bytes = 3  # 4-byte character
+            else:
+                return False  # Invalid leading byte
         else:
-            # Continuation byte check: it should be of the form 10xxxxxx
+            # Check if the byte is a valid continuation byte (10xxxxxx)
             if not (byte & mask1 and not (byte & mask2)):
                 return False
-
         num_bytes -= 1
 
     return num_bytes == 0
