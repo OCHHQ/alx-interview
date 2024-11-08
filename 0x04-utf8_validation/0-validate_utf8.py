9f22a551660278d50bc39f8fa61e2b870731dc45
OCHHQ <enosejecollins@gmail.com>
2024-11-08 15:45:04 +0100

    utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,27 +5,26 @@ Module to validate if a given data set represents a valid UTF-8 encoding.
 
 
 def validUTF8(data):
-    # The number of bytes remaining for the current UTF-8 character
-    num_bytes = 0
+    num_bytes = 0  # Number of bytes left to process for a character
 
     # Masks to check the structure of each byte
     mask1 = 1 << 7  # 10000000
     mask2 = 1 << 6  # 01000000
 
     for number in data:
-        # Only use the 8 least significant bits of the number
+        # Only consider the last 8 bits of the number (1 byte)
         byte = number & 0xFF
 
-        if num_bytes == 0:
-            # Determine the number of bytes in the UTF-8 character
-            if byte & mask1 == 0:
-                continue  # 1-byte character: 0xxxxxxx
-            elif byte & (mask1 >> 1) == mask1:
-                num_bytes = 1  # 2-byte character: 110xxxxx
-            elif byte & (mask1 >> 2) == mask1:
-                num_bytes = 2  # 3-byte character: 1110xxxx
-            elif byte & (mask1 >> 3) == mask1:
-                num_bytes = 3  # 4-byte character: 11110xxx
+        if num_bytes == 0:  # No ongoing multi-byte character
+            # Determine the number of bytes in the current UTF-8 character
+            if byte & mask1 == 0:  # 1-byte character: 0xxxxxxx
+                continue
+            elif byte & (mask1 >> 1) == mask1:  # 2-byte character: 110xxxxx
+                num_bytes = 1
+            elif byte & (mask1 >> 2) == mask1:  # 3-byte character: 1110xxxx
+                num_bytes = 2
+            elif byte & (mask1 >> 3) == mask1:  # 4-byte character: 11110xxx
+                num_bytes = 3
             else:
                 return False  # Invalid leading byte
         else:
@@ -34,4 +33,4 @@ def validUTF8(data):
                 return False
             num_bytes -= 1
 
-    return num_bytes == 0
+    return num_bytes == 0  # Return True if all bytes are processed correctly
