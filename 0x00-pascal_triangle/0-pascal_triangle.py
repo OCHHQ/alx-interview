3faefbae87e505f5c879d874d97e0d218f00c671
Collins Enoseje <enosejecollins@gmail.com>
2024-11-09 13:10:08 +0100

    Merge remote changes and add N Queens solution

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -13,14 +13,11 @@ def pascal_triangle(n):
     triangle = []
 
     for i in range(n):
-        # Initialize the row with 1s
         row = [1] * (i + 1)
 
-        # Update the elements inside the row, starting from the second row (i >= 2)
         for j in range(1, i):
             row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
 
-        # Add the row to the triangle
         triangle.append(row)
 
     return triangle
--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,83 +1,110 @@
 #!/usr/bin/python3
 """
-N Queens solver - Places N non-attacking queens on an NxN chessboard
-Usage: nqueens N
-    - N must be an integer >= 4
+N Queens Problem Solver
+The N queens puzzle is to place N non-attacking queens on an N×N chessboard.
+This solution prints all possible solutions for a given N.
 """
 import sys
 
 
 def is_safe(board, row, col, n):
     """
-    Check if a queen can be placed on board[row][col]
-    
+    Determine if a queen can be placed on board[row][col]
+
     Args:
-        board: The current board state represented as a list of queen positions
+        board: List of lists representing the board
         row: Row to check
         col: Column to check
         n: Size of the board
-    
+
     Returns:
-        Boolean indicating if the position is safe
+        Boolean: True if safe, False otherwise
     """
-    # Check if there is a queen in the same column
-    for i in range(row):
-        if board[i][1] == col:
+    # Check row on left side
+    for j in range(col):
+        if board[row][j] == 1:
             return False
 
     # Check upper diagonal on left side
-    for i, j in zip(range(row-1, -1, -1),
-                   range(col-1, -1, -1)):
-        if i >= 0 and j >= 0 and board[i][1] == j:
+    i, j = row, col
+    while i >= 0 and j >= 0:
+        if board[i][j] == 1:
             return False
+        i -= 1
+        j -= 1
 
-    # Check upper diagonal on right side
-    for i, j in zip(range(row-1, -1, -1),
-                   range(col+1, n)):
-        if i >= 0 and j < n and board[i][1] == j:
+    # Check lower diagonal on left side
+    i, j = row, col
+    while j >= 0 and i < n:
+        if board[i][j] == 1:
             return False
+        i += 1
+        j -= 1
 
     return True
 
 
+def solve_nqueens_util(board, col, n, solutions):
+    """
+    Recursive utility function to solve N Queens problem
+
+    Args:
+        board: List of lists representing the board
+        col: Current column
+        n: Size of the board
+        solutions: List to store all solutions
+    """
+    if col >= n:
+        solution = []
+        for i in range(n):
+            for j in range(n):
+                if board[i][j] == 1:
+                    solution.append([i, j])
+        solutions.append(solution)
+        return
+
+    for i in range(n):
+        if is_safe(board, i, col, n):
+            board[i][col] = 1
+            solve_nqueens_util(board, col + 1, n, solutions)
+            board[i][col] = 0
+
+
 def solve_nqueens(n):
     """
-    Solve the N Queens problem and print all solutions
-    
+    Initialize the board and solve N Queens problem
+
     Args:
-        n: Size of the board and number of queens
+        n: Size of the board
     """
-    def solve_util(board, row):
-        # Base case: If all queens are placed, print the solution
-        if row == n:
-            print(board)
-            return
+    # Initialize empty board
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    solutions = []
 
-        # Try placing queen in all columns of this row
-        for col in range(n):
-            if is_safe(board, row, col, n):
-                board[row][1] = col
-                solve_util(board, row + 1)
+    solve_nqueens_util(board, 0, n, solutions)
 
-    # Initialize the board with first column as row numbers
-    # and second column as queen positions (to be filled)
-    board = [[i, 0] for i in range(n)]
-    solve_util(board, 0)
+    # Print all solutions
+    for solution in solutions:
+        print(solution)
 
 
 def main():
-    """Main function to handle input and start solving"""
+    """
+    Main function to handle input and start solving
+    """
     # Check number of arguments
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
 
+    # Check if N is a number
     try:
         n = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
 
+    # Check if N is at least 4
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
